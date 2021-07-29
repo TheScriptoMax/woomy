bcd254cf41a6b38a8a60ede65e70b2160ed9d5e6
Chloé Sadry <chloesadry@Cefimmac48.local>
2021-07-29 09:47:47 +0200

    no change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto';
+import CowalkingCreate from './components/CowalkingCreate/CowalkingCreate';
+// import IconButton from '@material-ui/core/IconButton';
+// import DeleteIcon from '@material-ui/icons/Delete';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton> */}
+      <CowalkingCreate />
     </div>
   );
 }
---
+++ src/components/CowalkingCreate/CowalkingCreate.js
@@ -0,0 +1,10 @@
+import './CowalkingCreate.css';
+import TextField from '@material-ui/core/TextField';
+
+
+export default function CowalkingCreate () {
+    //objet à importer
+    return (
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    );
+  }
