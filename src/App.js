59c718357fb6e84b467df29b04dfd8c9419ac2b6
Bertrand Bouland <bbformationcefim@gmail.com>
2021-08-24 12:11:27 +0200

    FIRST PUSH

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-
 /// ----- COMPONENTS ----- ///
 
-    /**** HEADER & FOOTER *****/ 
-    import Header from './components/header/header';
-    import Footer from './components/footer/footer';
+/**** AUTHPROVIDER *****/
+import {AuthProvider} from "./contexts/AuthContext";
 
-    /**** CONNEXION *****/ 
-    import Login from './components/Login/Login';
-    import SignIn from './components/SignIn/SignIn';
+/**** HEADER & FOOTER *****/
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
-    /**** PROFIL *****/ 
-    import Account from './components/Account/Account';
-    import Params from './components/Params/Params';
+/**** CONNEXION *****/
+import Login from './components/Login/Login';
+import SignIn from './components/SignIn/SignIn';
 
-    /**** CRUD COPIETONNAGE *****/ 
-    import CowalkingList from './components/CowalkingList/CowalkingList';
-    import CowalkingTicket from './components/CowalkingTicket/CowalkingTicket';
-    import CowalkingCreate from './components/CowalkingCreate/CowalkingCreate';
-    import CowalkingSearch from './components/CowalkingSearch/CowalkingSearch';
-    
-    /**** NOTIFICATION *****/ 
-    import Notification from './components/Notifications/Notification';
+/**** PROFIL *****/
+import Account from './components/Account/Account';
+import Params from './components/Params/Params';
+
+/**** CRUD COPIETONNAGE *****/
+import CowalkingList from './components/CowalkingList/CowalkingList';
+import CowalkingTicket from './components/CowalkingTicket/CowalkingTicket';
+import CowalkingCreate from './components/CowalkingCreate/CowalkingCreate';
+import CowalkingSearch from './components/CowalkingSearch/CowalkingSearch';
+
+/**** NOTIFICATION *****/
+import Notification from './components/Notifications/Notification';
 
 /// ----- CSS ----- ///
 import './App.css';
@@ -28,29 +30,35 @@ import './App.css';
 /// ----- React Modules ----- ///
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 
-  return (
-    <Router>
-      <div>
-        <Header/>
-          <Switch>
-            <Route exact path='/ticket' component={CowalkingTicket}/>
-            <Route exact path='/create' component={CowalkingCreate}/>
-            <Route exact path='/search' component={CowalkingSearch}/>
-            <Route exact path='/account' component={Account}/>
-            <Route exact path='/message' component={Notification}/>
-            <Route exact path='/param' component={Params}/>
-            <Route exact path='/list' component={CowalkingList}/>
-          </Switch>
-        <Footer/>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+
+            <AuthProvider>
+                <Header/>
+                <Switch>
+                    <Route exact path='/ticket' component={CowalkingTicket}/>
+                    <Route exact path='/create' component={CowalkingCreate}/>
+                    <PrivateRoute exact path='/search' component={CowalkingSearch}/>
+                    <Route path='/signin' component={SignIn}/>
+                    <Route path='/login' component={Login}/>
+                    <Route exact path='/account' component={Account}/>
+                    <Route exact path='/message' component={Notification}/>
+                    <Route exact path='/param' component={Params}/>
+                    <Route exact path='/list' component={CowalkingList}/>
+                </Switch>
+                <Footer/>
+
+            </AuthProvider>
+        </Router>
+    );
 }
+
 export default App;
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,46 @@
+import {auth} from '../firebase';
+import {createContext, useContext, useEffect, useState} from "react";
+
+const AuthContext = createContext(undefined);
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    function logout() {
+        return auth.signOut()
+    }
+
+    useEffect(()=> {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        })
+        return unsubscribe;
+    }, []);
+
+    const value ={
+        currentUser,
+        signup,
+        login,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
