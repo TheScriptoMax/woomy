cf2a8db7e704d3108ccfec8e46c593eb1094d25e
Clemvr <c.vanrech@gmail.com>
2021-08-25 17:13:00 +0200

    select admin and margin create

--- src/components/AdminPlace/AdminPlace.jsx
+++ src/components/AdminPlace/AdminPlace.jsx
@@ -1,6 +1,6 @@
 import './adminplace.css'
 import {Button, TextField} from '@material-ui/core';
-
+import { MenuItem } from '@material-ui/core';
 
 function AdminPlace () {
     return (
@@ -10,7 +10,9 @@ function AdminPlace () {
          <p className="placecreate">Création d'un nouveau lieu</p>
          <form className="placeform">
             <TextField label="Lieux" variant="outlined"/>
-            <TextField label="Quartier" variant="outlined"/>
+            <TextField label="Quartier" variant="outlined" select>
+                <MenuItem></MenuItem>
+            </TextField>
             <TextField label="adresse approx" variant="outlined"/>
             <Button variant='contained'>submit</Button>
          </form>
