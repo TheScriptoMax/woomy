65c96125d7fd47a9945edabf30b382fa77a6e813
Clemvr <c.vanrech@gmail.com>
2021-08-27 15:29:28 +0200

    admin added

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -6,10 +6,12 @@ function Admin () {
     return (
      <div class="container container-admin">
          <h1>Admin</h1>
-         <Link to="/adminplace">
+         
             <Button variant="contained">Utilisateurs</Button>
+       
+         <Link to="/adminplace">
+            <Button variant="contained">Lieux</Button>
          </Link>
-         <Button variant="contained">Lieux</Button>
      </div>
     )
 };
