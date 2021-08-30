0b10229bef2105849a27466f85891231c37a3bf1
Bertrand Bouland <bbformationcefim@gmail.com>
2021-08-30 09:20:13 +0200

    Rename footer et header

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@
 import {AuthProvider} from "./contexts/AuthContext";
 
 /**** HEADER & FOOTER *****/
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 /**** LEGAL *****/
