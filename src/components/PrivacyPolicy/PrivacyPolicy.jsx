fc04903e9d618d88edd3f99bcf669c5d69c89722
Max <themax41@hotmail.fr>
2021-09-01 12:36:24 +0200

    gestion erreurs

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,23 +1,15 @@
-// IMPORT FIREBASE
-import {useAuth} from "../../contexts/AuthContext";
 
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+
+
 
 
 export default function PrivacyPolicy () {
 
 
-    const {currentUser} = useAuth();
-
 
     return (
 
         <div>
-            {currentUser && <Header />}
-            <h1>Politique de confidentialité</h1>
-                    <p></p>
-            {currentUser && <Footer/>}
         </div>
     );
 }
